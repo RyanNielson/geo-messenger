8751b6152208531e9653faae659f9ba0b4072ee9
TJ Webb <webb.tj@gmail.com>
2013-11-09 18:46:01 -0400

    geolocation change polling tolerance - only register changes when they exceed a set tolerance

--- public/javascripts/geo.js
+++ public/javascripts/geo.js
@@ -1,13 +1,23 @@
 var Geo = function() {
 };
 
+Geo.prototype.changeTolerance = 0.0001; // approx 7.8m
+
 Geo.prototype.current_location = function(position) {
   var coords = position.coords;
-  this.location.lat = coords.latitude;
-  this.location.lon = coords.longitude;
-  this.location_established = true;
-  console.log("You're now at Lat: " + coords.latitude + " & Lon: " + coords.longitude);
-  socket.emit('locationChange', { coords: coords });
+  var dLat = Math.abs(this.point.lat - coords.latitude);
+  var dLon = Math.abs(this.point.lon - coords.longitude);
+  if(dLat >= this.changeTolerance || dLon >= this.changeTolerance || this.point.lat === false || this.point.lon === false) {
+    this.point.lat = coords.latitude;
+    this.point.lon = coords.longitude;
+    this.location_established = true;
+    console.log("You're now at Lat: " + coords.latitude + " & Lon: " + coords.longitude);
+    alert("You're now at Lat: " + coords.latitude + " & Lon: " + coords.longitude);
+    socket.emit('locationChange', { coords: coords });
+  }
+  else{
+    alert("You coords changed, but not that much. Delta Lat: " + dLat + ", Delta Lon: " + dLon);
+  }
 };
 
 Geo.prototype.error = function(err) {
@@ -23,7 +33,7 @@ Geo.prototype.default_options = {
   timeout: 5000
 };
 
-Geo.prototype.location = {
+Geo.prototype.point = {
   lat: false,
   lon: false
 };
@@ -31,8 +41,16 @@ Geo.prototype.location = {
 Geo.prototype.location_established = false;
 
 Geo.prototype.track_location = function(){
-  if(Modernizr.geolocation)
-      navigator.geolocation.watchPosition(this.current_location, this.error, this.default_options);
+  if(Modernizr.geolocation){
+      _this = this;
+      navigator.geolocation.watchPosition(
+        function(position){
+          _this.current_location(position);
+        },
+        this.error,
+        this.default_options
+      );
+  }
   else
       this.error();
 }
