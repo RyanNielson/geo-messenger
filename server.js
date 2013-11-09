21f970a8cf84e3198cf69540e19485903c4a47d4
TJ Webb <webb.tj@gmail.com>
2013-11-08 23:45:41 -0400

    static file serving, including additional node modules

--- server.js
+++ server.js
@@ -1,26 +1,61 @@
 // https://github.com/nko4/website/blob/master/module/README.md#nodejs-knockout-deploy-check-ins
 require('nko')('8d-JzsAEQb_z3nnq');
-
 var isProduction = (process.env.NODE_ENV === 'production');
-var http = require('http');
 var port = (isProduction ? 80 : 8000);
 
-http.createServer(function (req, res) {
-  // http://blog.nodeknockout.com/post/35364532732/protip-add-the-vote-ko-badge-to-your-app
-  var voteko = '<iframe src="http://nodeknockout.com/iframe/the-nic-cages" frameborder=0 scrolling=no allowtransparency=true width=115 height=25></iframe>';
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var mime = require('mime');
+var cache = {};
 
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<html><body>' + voteko + '</body></html>\n');
-}).listen(port, function(err) {
-  if (err) { console.error(err); process.exit(-1); }
+var server = http.createServer(function(request, response){
+  var filePath = false;
+  if(request.url == '/')
+    filePath = 'public/index.html';
+  else
+    filePath = 'public' + request.url;
+  
+  var absPath = './' + filePath;
+  serveStatic(response, cache, absPath);
+});
 
-  // if run as root, downgrade to the owner of this file
-  if (process.getuid() === 0) {
-    require('fs').stat(__filename, function(err, stats) {
-      if (err) { return console.error(err); }
-      process.setuid(stats.uid);
+server.listen(port, function(err){
+  if(err){
+    console.error(err);
+    process.exit(-1);
+  }
+  console.log('listening on port ' + port);
+});
+
+function send404(response){
+  response.writeHead(404, {'Content-Type': 'text/plain'});
+  response.write('Error 404: resource not found.');
+  response.end();
+}
+
+function sendFile(response, filePath, fileContents){
+  response.writeHead(200, {"content-type": mime.lookup(path.basename(filePath))});
+  response.end(fileContents);
+}
+
+function serveStatic(response, cache, absPath){
+  if(cache[absPath])
+    sendFile(response, absPath, cache[absPath]);
+  else{
+    fs.exists(absPath, function(exists){
+      if(exists){
+        fs.readFile(absPath, function(err, data){
+          if(err)
+            send404(response);
+          else{
+            cache[absPath] = data;
+            sendFile(response, absPath, data);
+          }
+        });
+      }
+      else
+        send404(response);
     });
   }
-
-  console.log('Server running at http://0.0.0.0:' + port + '/');
-});
+}
