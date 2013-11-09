9beea444f23bdfb3da8453ff18e0bcdbfe6dba3f
Ryan Nielson <ryan.nielson@gmail.com>
2013-11-09 13:57:02 -0400

    Tweaked code formatting

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var cache = {};
 
 var server = http.createServer(function(request, response){
   var filePath = false;
-  if(request.url == '/')
+  if (request.url == '/')
     filePath = 'public/index.html';
   else
     filePath = 'public' + request.url;
@@ -21,41 +21,44 @@ var server = http.createServer(function(request, response){
 });
 
 server.listen(port, function(err){
-  if(err){
+  if (err) {
     console.error(err);
     process.exit(-1);
   }
   console.log('listening on port ' + port);
 });
 
-function send404(response){
+function send404(response) {
   response.writeHead(404, {'Content-Type': 'text/plain'});
   response.write('Error 404: resource not found.');
   response.end();
 }
 
-function sendFile(response, filePath, fileContents){
+function sendFile(response, filePath, fileContents) {
   response.writeHead(200, {"content-type": mime.lookup(path.basename(filePath))});
   response.end(fileContents);
 }
 
-function serveStatic(response, cache, absPath){
-  if(isProduction && cache[absPath])
+function serveStatic(response, cache, absPath) {
+  if (isProduction && cache[absPath]) {
     sendFile(response, absPath, cache[absPath]);
-  else{
-    fs.exists(absPath, function(exists){
-      if(exists){
+  }
+  else {
+    fs.exists(absPath, function(exists) {
+      if(exists) {
         fs.readFile(absPath, function(err, data){
-          if(err)
+          if(err) {
             send404(response);
-          else{
+          }
+          else {
             cache[absPath] = data;
             sendFile(response, absPath, data);
           }
         });
       }
-      else
+      else {
         send404(response);
+      }
     });
   }
 }
